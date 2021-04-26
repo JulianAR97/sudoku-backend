ee8c8abd25515ec70442612ad0e8b98694b5d377
Julian <julianrosenthal@yahoo.com>
2021-04-26 11:41:14 -0700

    added to get_stats method

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -4,12 +4,12 @@ module StatsHelper
     user = User.find_by(id: user_id)
     if user 
       stats['gamesFinished'] = user.scores.total
-      stats['totalByDifficulty'] = {}
-      stats['averageByDifficulty'] = {}
+      stats['totalsByDifficulty'] = {}
+      stats['averagesByDifficulty'] = {}
       stats['topTenByDifficulty'] = {}
       %w[easy medium hard expert].each do |d|
-        stats['totalByDifficulty'][d] = user.scores.total_by_difficulty(d)
-        stats['averageByDifficulty'][d] = user.scores.average_by_difficulty(d)
+        stats['totalsByDifficulty'][d] = user.scores.total_by_difficulty(d)
+        stats['averagesByDifficulty'][d] = user.scores.average_by_difficulty(d)
         stats['topTenByDifficulty'][d] = user.scores.top_ten_by_difficulty(d).map(&:time)
       end
     end
