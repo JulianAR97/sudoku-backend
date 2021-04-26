d2496a4f7cb7178425c829935411187fad7e9e16
Julian <julianrosenthal@yahoo.com>
2021-04-25 19:14:25 -0700

    added to_stopwatch method to convert secs to stopwatch ex: 05:04;

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,26 +1,34 @@
 class Score < ApplicationRecord
   belongs_to :user
+  validates :difficulty, inclusion: { in: %w[easy medium hard expert] }
+  # time refers to time taken to complete puzzle, not timestamp
+  validates :time, presence: true
+
   scope :total, -> { count('*') }
   scope :total_by_difficulty, ->(difficulty) { where(difficulty: difficulty).count('*') }
   scope :top_ten_by_difficulty, ->(difficulty) { where(difficulty: difficulty).order('time').limit(10) }
-  
+
   def self.average_by_difficulty(difficulty)
     times = where(difficulty: difficulty).map(&:time)
+
     # Avoid times by 0 error
-    return '' if (times.length === 0)
+    return '' if times.length === 0
+
     total_time = 0
     times.each do |time|
       mins_secs = time.split(':')
       total_time += (mins_secs[0].to_i * 60) + (mins_secs[1].to_i)
     end
     average = total_time / times.size
-    "#{average / 60}:#{average % 60}"
+    secs_to_stopwatch(average)
   end
-
-
-  validates :difficulty, inclusion: { in: %w[easy medium hard expert] }
-  # time refers to time taken to complete puzzle, not timestamp
-  validates :time, presence: true
+  
+  def self.secs_to_stopwatch(secs)
+    mins_sw = secs / 60 < 10 ? "0#{secs / 60}" : secs / 60
+    secs_sw = secs % 60 < 10 ? "0#{secs % 60}" : secs % 60
+    "#{mins_sw}:#{secs_sw}"
+  end
+  
 end
 
 
