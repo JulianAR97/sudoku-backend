706c4e6f84275178accdf0be7069cc28ece8aaaf
Julian <julianrosenthal@yahoo.com>
2021-04-25 19:02:32 -0700

    added top_ten_by_difficulty to stats object returned by get_stats function

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -5,8 +5,12 @@ module StatsHelper
     if user 
       stats['gamesFinished'] = user.scores.total
       stats['totalByDifficulty'] = {}
+      stats['averageByDifficulty'] = {}
+      stats['topTenByDifficulty'] = {}
       %w[easy medium hard expert].each do |d|
         stats['totalByDifficulty'][d] = user.scores.total_by_difficulty(d)
+        stats['averageByDifficulty'][d] = user.scores.average_by_difficulty(d)
+        stats['topTenByDifficulty'][d] = user.scores.top_ten_by_difficulty(d).map(&:time)
       end
     end
     stats
