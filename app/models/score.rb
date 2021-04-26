b3719d45f32b1cac5e001631e3471ebaef1552c3
Julian <julianrosenthal@yahoo.com>
2021-04-25 17:28:40 -0700

    added scope methods

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,9 @@
 class Score < ApplicationRecord
   belongs_to :user
-  scope :top_ten, -> { order('time').limit(10) }
+  scope :total, -> { count('*') }
+  scope :total_by_difficulty, ->(difficulty) { where(difficulty: difficulty).count('*') }
+  scope :top_ten_by_difficulty, ->(difficulty) { where(difficulty: difficulty).order('time').limit(10) }
+
 
   validates :difficulty, inclusion: { in: %w[easy medium hard expert] }
   # time refers to time taken to complete puzzle, not timestamp
